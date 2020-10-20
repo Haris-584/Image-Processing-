ce9b5837e3de6c78552cc0fd48523ec19852a2c5
Muhammad Haris <36724032+Haris-584@users.noreply.github.com>
2020-10-20 11:11:22 +0500

    initial commit

---
+++ read_resize_show.m
@@ -0,0 +1,33 @@
+% read image
+im=imread('image.jpg');
+
+% convert it into grayscale
+im=rgb2gray(imread('image.jpg'));
+
+% resize the image 
+im1=imresize(im, [1024 1024]);
+im2=imresize(im1, [1024 1024]/2);
+im3=imresize(im1, [1024 1024]/4);
+im4=imresize(im1, [1024 1024]/8);
+im5=imresize(im1, [1024 1024]/16);
+im6=imresize(im1, [1024 1024]/32);
+
+% show the image in seperates windows
+figure;imshow(im1)
+figure;imshow(im2)
+figure;imshow(im3)
+figure;imshow(im4)
+figure;imshow(im5)
+
+% Combine all the results
+% (2,3,1) 2 rows 3 colums and 1 represent first position
+figure;
+subplot(2,3,1);imshow(im1);subplot(2,3,2);imshow(im2)
+subplot(2,3,3);imshow(im3);subplot(2,3,4);imshow(im4)
+subplot(2,3,5);imshow(im5);subplot(2,3,6);imshow(im6)
+
+figure;
+subplot(2,4,1);imshow(im1,[]);subplot(2,4,2);imshow(im2,[])
+subplot(2,4,3);imshow(im3,[]);subplot(2,4,4);imshow(im4,[])
+subplot(2,4,5);imshow(im5,[]);subplot(2,4,6);imshow(im6,[])
+subplot(2,4,7);imshow(im7,[]);subplot(2,4,8);imshow(im8,[])
